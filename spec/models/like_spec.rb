e75f97c14934d1a93aa14fb5eaa4307a6656d7e3
principles31 <principles31@gmail.com>
2022-08-29 21:36:30 +0000

    Add views for each nested route

--- spec/models/like_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Like, type: :model do
-  subject { Like.new }
-  before { subject.save }
-  it 'No post should raise error' do
-    expect { subject.likes_counter }.to raise_error(NoMethodError)
-  end
-end
