c34119e8d3564e7cba4b4249054b30e1ae125993
principles31 <principles31@gmail.com>
2022-08-31 09:02:29 +0000

    Fix trailling white spaces

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,10 @@
 class LikesController < ApplicationController
   def create
-      @like = Like.new(like_params)
+    @like = Like.new(like_params)
       redirect_to user_posts_path(id: @like.post_id, user_id: @like.author_id) if @like.save
-    end
-  
+  end
+
   def like_params
-      params.require(:like).permit(:post_id, :author_id)
-    end
+    params.require(:like).permit(:post_id, :author_id)
+  end
 end
