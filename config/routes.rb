aa538bb933e770ff4cb7d2c31092c15367885139
principles31 <principles31@gmail.com>
2022-09-06 14:24:57 +0000

    Add devise email authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+  devise_for :users
+  root 'users#index'
+
+  resources :users, only: %I[index show] do
+    resources :posts, only: %I[index show new create]
   end
 
   resources :posts do
     resources :comments, only: %i[create]
     resources :likes, only: %i[create]
+  end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 end
